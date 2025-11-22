cd23195fd1684b2eca09683c026a69cdfa0a7e53
Arielle <aturullo@andrew.cmu.edu>
2025-11-22 00:53:53 -0500

    adding my first file

---
+++ first-user-story.py
@@ -0,0 +1,25 @@
+# create user story for cancel order
+
+# Cancel order
+# Customer
+# To cancel my order before a shopper begins shopping
+# I can receive a full refund if my plans change.
+# Complex
+# Operational
+
+# want to use trigger and tables to "cancel order" if requested
+# what does it mean to cancel? 
+
+# user --> places order
+
+# user --> pays for total ConnectionResetError
+
+# user --> cancels order
+
+# total cost --> restored to 0 in transactions
+
+
+# (show as getting negative balance? )
+
+
+# this is my change 
