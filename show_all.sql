e4bdf147d529bc2de23fcbd192aaf9a029532afc
Arielle <aturullo@andrew.cmu.edu>
2025-12-05 13:30:42 -0500

    updating show_all_sql and deleting old create

--- show_all.sql
+++ show_all.sql
@@ -1,22 +1,50 @@
 \c project
 
-\echo 'Cart Table'
-SELECT * FROM Cart;
+\echo 'Addresses Table'
+SELECT * FROM Addresses;
 
-\echo 'Customer Table'
-SELECT * FROM Customer;
+\echo 'Users Table'
+SELECT * FROM Users';
+
+\echo 'Customers Table'
+SELECT * FROM Customers;
+
+\echo 'Shoppers Table'
+SELECT * FROM Shoppers;
+
+\echo 'Batches Table'
+SELECT * FROM Batches;
+
+\echo 'Stores Table'
+SELECT * FROM Stores;
+
+\echo 'Lists Table'
+SELECT * FROM Lists;
+
+\echo 'Carts Table'
+SELECT * FROM Carts;
+
+\echo 'Categories Table'
+SELECT * FROM Categories;
+
+\echo 'Items Table'
+SELECT * FROM Items;
 
 \echo 'Orders Table'
 SELECT * FROM Orders;
 
-\echo 'Payment Table'
-SELECT * FROM Payment;
+\echo Cart_Items Table'
+SELECT * FROM Cart_Items;
 
-\echo 'Store Table'
-SELECT * FROM Store;
+\echo 'List_Items Table'
+SELECT * FROM List_Items;
 
-\echo 'Users Table'
-SELECT * FROM Users;
+\echo 'Store_Items Table'
+SELECT * FROM Store_Items;
 
-\echo 'ZipCodeZones Table'
-SELECT * FROM ZipCodeZones;
+\echo 'Store_Categories Table'
+SELECT * FROM Store_Categories;
+
+\echo 'Item_Categories Table'
+SELECT * FROM Item_Categories;
+
